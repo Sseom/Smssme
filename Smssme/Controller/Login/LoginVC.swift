a2d4b51baecc07b8a2980d360d760d1e18187e4f
jihyeon <dkswlgus0314@naver.com>
2024-08-27 23:49:44 +0900

    [ui]: Login UI 작업 - 기능 구현 추가 예정

---
+++ Smssme/Controller/Login/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smssme
+//
+//  Created by ahnzihyeon on 8/27/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    private let loginVeiw = LoginView()
+    
+    override func loadView() {
+        view = loginVeiw
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
