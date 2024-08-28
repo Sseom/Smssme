abffda158d4cf9003f236dd0cfe1ac8b2a2351cc
bunabloom <kimrinoffdev@gmail.com>
2024-08-28 23:35:49 +0900

    #5 Feat: 켈린더 제작

--- Smssme/Controller/TabBarController.swift
+++ Smssme/Controller/TabBarController.swift
@@ -20,7 +20,7 @@ class TabBarController: UITabBarController {
         guard let selectImage = UIImage(systemName: "multiply.circle.fill") else { return }
         
         let mainPage = tabBarNavigationController(unselectedImage: unselectImage, selectedImage: selectImage, isNavigationBarHidden: true, rootViewController: FinancialPlanSelectionVC(financialPlanSelectionView: FinancialPlanSelectionView()))
-        let diary = tabBarNavigationController(unselectedImage: unselectImage, selectedImage: selectImage, isNavigationBarHidden: false, rootViewController: FinancialPlanSelectionVC(financialPlanSelectionView: FinancialPlanSelectionView()))
+        let diary = tabBarNavigationController(unselectedImage: unselectImage, selectedImage: selectImage, isNavigationBarHidden: false, rootViewController: MoneyDiaryVC(moneyDiaryView: MoneyDiaryView()))
         let financialPlan = tabBarNavigationController(unselectedImage: unselectImage, selectedImage: selectImage, isNavigationBarHidden: false, rootViewController: FinancialPlanSelectionVC(financialPlanSelectionView: FinancialPlanSelectionView()))
         //        let myPage = tabBarNavigationController(unselectedImage: unselectImage, selectedImage: selectImage, isNavigationBarHidden: false, rootViewController: FinancialPlanSelectionVC(financialPlanSelectionView: FinancialPlanSelectionView()))
         
