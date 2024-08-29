a787a913bfc26fb98d8cdfa3f6ed8e7ab2ee3d60
jihyeon <dkswlgus0314@naver.com>
2024-08-29 17:08:25 +0900

    중간 커밋

---
+++ Smssme/Controller/Login/MypageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MypageViewController.swift
+//  Smssme
+//
+//  Created by ahnzihyeon on 8/29/24.
+//
+
+import UIKit
+
+class MypageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
---
+++ Smssme/View/Login/Mypage.swift
@@ -0,0 +1,13 @@
+//
+//  Mypage.swift
+//  Smssme
+//
+//  Created by ahnzihyeon on 8/29/24.
+//
+
+import UIKit
+
+class Mypage: UIView {
+
+
+}
