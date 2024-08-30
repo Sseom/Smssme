52cb8902ea83a1212075f9faf6961eb8910c60b3
wjstjd120 <wjstjd120@naver.com>
2024-08-30 18:03:34 +0900

    [feat] 중간 커밋

--- Smssme/Controller/MoneyDiary/MoneyDiaryEditView.swift
+++ Smssme/Controller/MoneyDiary/MoneyDiaryEditView.swift
@@ -5,4 +5,22 @@
 //  Created by 전성진 on 8/30/24.
 //
 
-import Foundation
+import SnapKit
+import UIKit
+
+class MoneyDiaryEditView: UIView {
+    private let segmentControl: UISegmentedControl = {
+        let segmentControl = UISegmentedControl()
+        segmentControl.selectedSegmentIndex = 0
+        return segmentControl
+    }()
+    
+    // MARK: - View Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Smssme/View/MoneyDiary/MoneyDiaryEditVC.swift
+++ Smssme/View/MoneyDiary/MoneyDiaryEditVC.swift
@@ -5,4 +5,25 @@
 //  Created by 전성진 on 8/30/24.
 //
 
-import Foundation
+import UIKit
+
+class MoneyDiaryEditVC: UIViewController {
+    private let moneyDiaryEditView: MoneyDiaryEditView = MoneyDiaryEditView()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func loadView() {
+        super.loadView()
+        self.view = moneyDiaryEditView
+    }
+}
