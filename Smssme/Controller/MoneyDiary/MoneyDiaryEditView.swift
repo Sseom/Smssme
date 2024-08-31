e04500428b9528a8589197f42a4f882b7a3d11e6
wjstjd120 <wjstjd120@naver.com>
2024-08-31 16:18:09 +0900

    [feat] 머니다이어리 내역편집 UI

--- Smssme/Controller/MoneyDiary/MoneyDiaryEditView.swift
+++ Smssme/Controller/MoneyDiary/MoneyDiaryEditView.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MoneyDiaryEditView: UIView {
     private let segmentControl: UISegmentedControl = {
-        let segmentControl = UISegmentedControl()
+        let segmentControl = UISegmentedControl(items: ["수입", "지출"])
         segmentControl.selectedSegmentIndex = 0
         return segmentControl
     }()
@@ -18,9 +18,25 @@ class MoneyDiaryEditView: UIView {
     // MARK: - View Init
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Method
+    
+    // MARK: - Private Method
+    
+    private func setupUI() {
+        [segmentControl].forEach {
+            self.addSubview($0)
+        }
+        
+        segmentControl.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide)
+            $0.left.right.equalTo(self.safeAreaLayoutGuide).inset(10)
+        }
     }
 }
