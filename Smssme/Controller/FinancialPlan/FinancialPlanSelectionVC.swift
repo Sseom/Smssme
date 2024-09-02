a13619392fbf350c9740f65f25a3217887453e63
HjeongMM <mmhj0129@naver.com>
2024-09-02 23:35:56 +0900

    [etc] backup

--- Smssme/Controller/FinancialPlan/FinancialPlanSelectionVC.swift
+++ Smssme/Controller/FinancialPlan/FinancialPlanSelectionVC.swift
@@ -31,18 +31,18 @@ final class FinancialPlanSelectionVC: UIViewController {
     
     private func fetchItems() {
         planItems = [
-            PlanItem(title: "PlanItem1", description: "description1"),
-            PlanItem(title: "PlanItem2", description: "description2"),
-            PlanItem(title: "PlanItem3", description: "description3"),
-            PlanItem(title: "PlanItem4", description: "description4"),
-            PlanItem(title: "PlanItem5", description: "description4"),
+            PlanItem(title: "잊지못할 인생여행", description: "꿈꿔왔던 그곳을 가기위한 나의 인생여행 자금 마련 플랜"),
+            PlanItem(title: "드림카 프로젝트", description: "나만의 드림카를 갖는 그날을 위한 드림카플랜"),
+            PlanItem(title: "내집 마련의 꿈", description: "미래의 나의 보금자리를 위한 첫걸음, 내집마련 플랜"),
+            PlanItem(title: "로맨틱 결혼식", description: "새로운 삶의 시작인 그 행복한 순간을 위한 결혼자금 플랜"),
+            PlanItem(title: "황금빛 은퇴자금", description: "편안한 은퇴를 위한 준비, 빨리 시작할수록 든든합니다!"),
             
         ]
         financialPlanSelectionView.collectionView.reloadData()
     }
     
     @objc private func addButtonTapped() {
-        planItems.append(PlanItem(title: "New Plan", description: "New Description"))
+        planItems.append(PlanItem(title: "나만의 플랜 설정", description: "나만의 자산목표를 설정하고 체계적으로 이루어 보세요"))
         financialPlanSelectionView.collectionView.reloadData()
     }
 }
@@ -69,15 +69,7 @@ extension FinancialPlanSelectionVC: UICollectionViewDataSource {
             return cell
         }
     }
-    
 }
-
-//extension FinancialPlanSelectionVC: UICollectionViewDelegate {
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        let createPlanVC = FinancialPlanCreateVC(textFieldArea: CreateTextView())
-//        navigationController?.pushViewController(createPlanVC, animated: true)
-//    }
-//}
 
 extension FinancialPlanSelectionVC: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
