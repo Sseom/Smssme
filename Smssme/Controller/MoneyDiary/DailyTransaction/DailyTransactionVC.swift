754a2b826330975dcbba2e965ff65203094b4cac
jihyeon <dkswlgus0314@naver.com>
2024-09-05 19:32:56 +0900

    [commit]: pr 하기 위한 커밋

--- Smssme/Controller/MoneyDiary/DailyTransaction/DailyTransactionVC.swift
+++ Smssme/Controller/MoneyDiary/DailyTransaction/DailyTransactionVC.swift
@@ -28,9 +28,6 @@ class DailyTransactionVC: UIViewController {
         self.setupUI()
         view.backgroundColor = .white
     }
-    override func viewWillAppear(_ animated: Bool) {
-        self.transactionView.listCollectionView.reloadData()
-    }
     
     override func viewWillAppear(_ animated: Bool) {
         calculateTodayTransaction(items: transactionList)
