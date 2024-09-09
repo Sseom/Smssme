a43c9bd18fb619ffda31c2181a692d954435ab0d
HjeongMM <mmhj0129@naver.com>
2024-09-09 13:49:08 +0900

    [fix] 로그인창 네비게이션컨트롤러 제거

--- Smssme/View/Login/LoginView.swift
+++ Smssme/View/Login/LoginView.swift
@@ -9,11 +9,9 @@ import SnapKit
 import UIKit
 
 final class LoginView: UIView {
-    
     // 아이디, 비밀번호, 로그인 버튼의 공통된 높이
     private let textFieldHeight = 48
     
-    // 로고 이미지
     private let logoImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
@@ -21,7 +19,6 @@ final class LoginView: UIView {
         return imageView
     }()
     
-    // 아이디(이메일) 입력
     var emailTextField: UITextField = {
         var textField = UITextField()
         textField.placeholder = "아이디(이메일)을 입력해주세요."
@@ -33,7 +30,6 @@ final class LoginView: UIView {
         return textField
     }()
     
-    // 비밀번호 입력
     var passwordTextField: UITextField = {
         var textField = UITextField()
         textField.placeholder = "비밀번호를 입력해주세요."
@@ -46,8 +42,7 @@ final class LoginView: UIView {
         textField.textContentType = .oneTimeCode
         return textField
     }()
-    
-    // 로그인 버튼
+
     let loginButton = BaseButton().createButton(text: "로그인", color: UIColor.systemBlue, textColor: UIColor.white)
     
     // 아이디, 비밀번호, 로그인 stackView
@@ -59,14 +54,9 @@ final class LoginView: UIView {
         return stackView
     }()
     
-    //회원가입 버튼
     let signupButton = BaseButton().createButton(text: "회원가입", color: #colorLiteral(red: 0.9121415615, green: 0.9536862969, blue: 1, alpha: 1), textColor: UIColor.systemBlue)
     
-    
-    //비회원 로그인
     let unLoginButton = BaseButton().createButton(text: "로그인 없이 둘러보기", color: .clear, textColor: UIColor(hex: "#333333"))
-    
-    
     
     // 빈 화면 터치 시 키보드 내려감
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -83,37 +73,31 @@ final class LoginView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-    //MARK: - setupUI
+    //MARK: - Auto Layout
     private func configureUI() {
         self.backgroundColor = .white
         
-        // loginStackView에 추가
         [emailTextField, passwordTextField].forEach {loginStackView.addArrangedSubview($0)}
         
-        // view에 추가
         [logoImageView, loginStackView,loginButton, signupButton, unLoginButton].forEach {self.addSubview($0)}
         
     }
     
     private func setupLayout() {
-        // 로고 이미지
         logoImageView.snp.makeConstraints {
             $0.top.equalTo(safeAreaLayoutGuide.snp.top).offset(16)
-            $0.leading.equalToSuperview().offset(30)
-            $0.trailing.equalToSuperview().offset(-30)
+            $0.leading.equalToSuperview().offset(40)
+            $0.trailing.equalToSuperview().offset(-40)
             $0.height.equalTo(logoImageView.snp.width)
         }
         
-        // 아이디, 비밀번호 스택뷰
         loginStackView.snp.makeConstraints {
-            $0.top.equalTo(logoImageView.snp.bottom).offset(24)
+            $0.top.equalTo(logoImageView.snp.bottom).offset(16)
             $0.centerX.equalToSuperview()
-            $0.height.equalTo(textFieldHeight * 2 + 16) //스택뷰 spacing 간격 18
+            $0.height.equalTo(textFieldHeight * 2 + 16)
             $0.horizontalEdges.equalToSuperview().inset(30)
         }
         
-        // 로그인 버튼
         loginButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalTo(loginStackView.snp.bottom).offset(24)
@@ -121,7 +105,6 @@ final class LoginView: UIView {
             $0.height.equalTo(textFieldHeight)
         }
         
-        // 회원가입 버튼
         signupButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalTo(loginButton.snp.bottom).offset(8)
@@ -129,12 +112,10 @@ final class LoginView: UIView {
             $0.height.equalTo(textFieldHeight)
         }
         
-        // 비회원 로그인
         unLoginButton.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalTo(signupButton.snp.bottom).offset(24)
         }
-        
     }
     
     @objc
