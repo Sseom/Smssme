1be7c7a7b7056c68df4b5f35e8c79676a0b5ec8f
bloom <kimrinoffdev@gmail.com>
2024-09-12 14:28:00 +0900

    fix: fix gitgnore

---
+++ Smssme/Controller/MoneyDiary/AutomaticTransactionVC.swift
@@ -0,0 +1,13 @@
+//
+//  AutomaticTransactionVC.swift
+//  Smssme
+//
+//  Created by KimRin on 9/12/24.
+//
+
+import UIKit
+
+class AutomaticTransactionVC: UIViewController {
+    
+    
+}
---
+++ Smssme/View/MoneyDiary/AutomaticTransactionView.swift
@@ -0,0 +1,8 @@
+//
+//  AutomaticTransactionView.swift
+//  Smssme
+//
+//  Created by KimRin on 9/12/24.
+//
+
+import Foundation
