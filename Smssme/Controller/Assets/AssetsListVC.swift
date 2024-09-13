e63654ea948e145a06e4ff3659ea12e094c3ac1b
wjstjd120 <wjstjd120@naver.com>
2024-09-13 13:13:47 +0900

    [etc] 코드 재 작성

--- Smssme/Controller/Assets/AssetsListVC.swift
+++ Smssme/Controller/Assets/AssetsListVC.swift
@@ -45,61 +45,56 @@ class AssetsListVC: UIViewController {
     
     // MARK: - Private Method
     private func setButtonEvents() {
-        assetsListView.saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
-        assetsListView.cancelButton.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
     }
     
     private func setDeleteButton() {
-        assetsListView.deleteButton.target = self
-        assetsListView.deleteButton.action = #selector(deleteButtonTapped)
-        navigationItem.rightBarButtonItem = assetsListView.deleteButton
     }
     
     private func setData() {
-        if let uuid = uuid, let asset = assetsCoreDataManager.selectSelectAssets(uuid: uuid).first {
-            assetsListView.categoryTextField.text = asset.category
-            assetsListView.titleTextField.text = asset.title
-            assetsListView.amountTextField.text = "\(KoreanCurrencyFormatter.shared.string(from: asset.amount))"
-            assetsListView.noteTextField.textColor = .black
-            assetsListView.noteTextField.text = asset.note
-            setDeleteButton()
-        }
+//        if let uuid = uuid, let asset = assetsCoreDataManager.selectSelectAssets(uuid: uuid).first {
+//            assetsListView.categoryTextField.text = asset.category
+//            assetsListView.titleTextField.text = asset.title
+//            assetsListView.amountTextField.text = "\(KoreanCurrencyFormatter.shared.string(from: asset.amount))"
+//            assetsListView.noteTextField.textColor = .black
+//            assetsListView.noteTextField.text = asset.note
+//            setDeleteButton()
+//        }
     }
     
     private func saveAssets() {
-        let assets = AssetsItem(
-            category: assetsListView.categoryTextField.text,
-            title: assetsListView.titleTextField.text, 
-            amount: KoreanCurrencyFormatter.shared.number(from: assetsListView.amountTextField.text ?? "") ?? 0,
-            note: assetsListView.noteTextField.text
-        )
-        
-        if let uuid = uuid {
-            assetsCoreDataManager.updateAssets(assetsItem: assets, uuid: uuid)
-        } else {
-            assetsCoreDataManager.createAssets(assets: assets)
-        }
+//        let assets = AssetsItem(
+//            category: assetsListView.categoryTextField.text,
+//            title: assetsListView.titleTextField.text, 
+//            amount: KoreanCurrencyFormatter.shared.number(from: assetsListView.amountTextField.text ?? "") ?? 0,
+//            note: assetsListView.noteTextField.text
+//        )
+//        
+//        if let uuid = uuid {
+//            assetsCoreDataManager.updateAssets(assetsItem: assets, uuid: uuid)
+//        } else {
+//            assetsCoreDataManager.createAssets(assets: assets)
+//        }
     }
     
     private func deleteAssets() {
-        if let uuid = uuid {
-            assetsCoreDataManager.deleteAssets(uuid: uuid)
-        } else {
-            print("유효한 자산이 아닙니다.")
-        }
+//        if let uuid = uuid {
+//            assetsCoreDataManager.deleteAssets(uuid: uuid)
+//        } else {
+//            print("유효한 자산이 아닙니다.")
+//        }
     }
     
     // 뒤로가기전에 차트를 다시 그려주는 메서드
     private func popupViewController() {
-        if let mainPageVC = navigationController?.viewControllers.last(where: { $0 is MainPageVC }) as? MainPageVC {
-            
-            mainPageVC.setChartData()
-            
-            navigationController?.popViewController(animated: true)
-        } else {
-            // MainPageVC가 네비게이션 스택에 없는 경우 처리
-            print("MainPageVC를 찾을 수 없습니다.")
-        }
+//        if let mainPageVC = navigationController?.viewControllers.last(where: { $0 is MainPageVC }) as? MainPageVC {
+//            
+//            mainPageVC.setChartData()
+//            
+//            navigationController?.popViewController(animated: true)
+//        } else {
+//            // MainPageVC가 네비게이션 스택에 없는 경우 처리
+//            print("MainPageVC를 찾을 수 없습니다.")
+//        }
     }
 
     
