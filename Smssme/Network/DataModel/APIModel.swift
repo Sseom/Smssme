ead6872778c200e85dcf63f14b731522b25efcd4
jihyeon <dkswlgus0314@naver.com>
2024-09-24 01:49:09 +0900

    [commit]: 중간 백업용 - 메인페이지 컬렉션뷰 추가 - 메인페이지 주요 경제지수 api 연결 중

--- Smssme/Network/DataModel/APIModel.swift
+++ Smssme/Network/DataModel/APIModel.swift
@@ -7,19 +7,13 @@
 
 import Foundation
 
-//MARK: - 주요 경제지수 통합모델
-
-// 공통 프로토콜
-protocol FinancialData {
-    var name: String { get }   // 지수명
-}
+//MARK: - MainPageView에 사용될 주요 경제지수 통합모델
 
 //MARK: - 코스피 데이터 모델
 //공공데이터포털 - 금융위원회 API를 사용합니다.
 
-struct KOSPIResponse: Codable, FinancialData {
+struct KOSPIResponse: Codable {
     let response: Response
-    var name: String = "KOSPI"
 }
 
 struct Response: Codable {
@@ -43,27 +37,13 @@ struct KOSPIItem: Codable {
     let vs: String // 전일 대비 등락
     let fltRt: String // 등락률
     let mkp: String // 시가
-//    let hipr: String // 최고가
-//    let lopr: String // 최저가
-//    let trqu: String // 거래량
-//    let trPrc: String // 거래대금
-//    let lstgMrktTotAmt: String // 상장시가총액
-//    let lsYrEdVsFltRg: String // 전년 말 대비 등락폭
-//    let lsYrEdVsFltRt: String // 전년 말 대비 등락률
-//    let yrWRcrdHgst: String // 연중 최고가
-//    let yrWRcrdHgstDt: String // 연중 최고가 날짜
-//    let yrWRcrdLwst: String // 연중 최저가
-//    let yrWRcrdLwstDt: String // 연중 최저가 날짜
-//    let basPntm: String // 기준 시점
-//    let basIdx: String // 기준 지수
 }
 
 
 //MARK: -환율 데이터 모델
 //한국수출입은행 API를 사용합니다.
 
-struct ExchangeRate: Codable, FinancialData {
-    var name: String = "환율"
+struct ExchangeRate: Codable {
     let curCode: String? //통화 코드 (예: USD)
     let bkpr: String? // 은행 매매 기준율
     
@@ -86,8 +66,7 @@ struct ExchangeRate: Codable, FinancialData {
 
 //MARK: -S&P500 데이터 모델
 //FRED API를 사용합니다.
-struct SP500Response: Codable, FinancialData {
-    var name: String = "S&P500"
+struct SP500Response: Codable {
     let observations: [Observation] // 관측 데이터 배열
 }
 
