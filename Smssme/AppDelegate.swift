f9911aa606d58810a337bbbd6ff17c9a79593ce8
HjeongMM <mmhj0129@naver.com>
2024-10-04 12:25:15 +0900

    [etc] backup

--- Smssme/AppDelegate.swift
+++ Smssme/AppDelegate.swift
@@ -10,6 +10,7 @@ import FirebaseCore
 import UIKit
 
 @main
+
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
